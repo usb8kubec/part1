784b7f4533cd5936e1fc89a0699ab576fef2a36d
Hieu Nguyen <nth937@gmail.com>
2022-06-21 00:07:25 +0300

    ex1.3 ver2

--- ex1.3/index.js
+++ ex1.3/index.js
@@ -2,7 +2,7 @@ const getHashNow = () => {
   const now = new Date().toString()
   const randomHash = Math.random().toString(36).substr(2, 6)
 
-  console.log(new Date().toString(), ': ', randomHash)
+  console.log(new Date().toString(), ':ver2: ', randomHash)
 
   setTimeout(getHashNow, 5000)
 }
